d34d23b9c7077f6527303f2e0306478be774075c
Chromulent <75396296+Chromulent@users.noreply.github.com>
2022-12-05 15:53:30 -0600

    12022.12.05 @ 1553

--- Automation/Functions/Yes_or_No_Whiptail.sh
+++ Automation/Functions/Yes_or_No_Whiptail.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-"yes_no_whiptail" () {
+yes_no_whiptail () {
        if whiptail --yesno "Subscriber ${subname} has uploaded a new video entitled: ${title}, do you want to download this?" 20 60 ;then
          yt-dlp --format "$dlpformat" --verbose --force-ipv4 --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive /home/blackwood/Downloads/.mydownloads  --check-formats --concurrent-fragments 5  -o "$subdirout" --restrict-filenames  --throttled-rate 100K "$videoVar"  >> "$contentFile"
       else
