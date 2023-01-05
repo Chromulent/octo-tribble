4b66b158eb796c3a015f73bec6abc75f1b48b4e7
Chromulent <75396296+Chromulent@users.noreply.github.com>
2023-01-05 12:59:15 -0600

    12023.01.05 @ 1259

--- CLI/Transmutation/Convert_avi-mp4.sh
+++ CLI/Transmutation/Convert_avi-mp4.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-find . -name '*.avi' -exec sh -c 'ffmpeg -y -i $0 -x265-params crf=25 "${0%%.avi}.webm"' {} \;
+find . -name '*.avi' -exec sh -c 'ffmpeg -y -i "$0" -x265-params crf=25 "${0%%.avi}.webm"' {} \;
 
