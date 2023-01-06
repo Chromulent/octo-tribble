2da95b7c5b25e49af21c19ad5e19664b2ecdea5c
Chromulent <75396296+Chromulent@users.noreply.github.com>
2023-01-05 21:43:03 -0600

    12023.01.05 @ 2142

--- CLI/DeDupe/DeDupe_avi-webm.sh
+++ CLI/DeDupe/DeDupe_avi-webm.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+# Old function
+# if_avi_webm_del () {
+# for f in *.avi; do
+#  [ -e "${f%.*}.webm" ] && rm -rf "$f"
+#done
+# }
+
 
 if_avi_webm_del () {
-for f in *.avi; do
-  [ -e "${f%.*}.webm" ] && rm -rf "$f"
-done
-
-}
+  for f in *.avi; do
+    d="$(echo "${f%.*}".webm)"
+    [[ -f "$f" ]] && fsize=$(find "$f" -printf "%s") && if [[ "$fsize" -lt "500000"  ]] ;then rm -rf "$f" ; fi 
+    [[ -f "$d" ]] && dsize=$(find "$d" -printf "%s") && if [[ "$dsize" -lt "500000"  ]] ;then rm -rf "$d" ; fi
+    [ -e "${f%.*}.webm" ] && rm -rf "$f"
+  done
+ }
 
 
 
