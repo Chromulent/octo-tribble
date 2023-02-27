7986bbbc393b1b9519b7175ca2be73a40f859dbf
Chromulent <75396296+Chromulent@users.noreply.github.com>
2023-02-27 13:06:21 -0600

    12023.02.27 @ 1306

--- subscribers/All_Gas_No_Brakes.py
+++ subscribers/All_Gas_No_Brakes.py
@@ -6,7 +6,7 @@ import re
 channel = "https://www.youtube.com/channel/UCtqxG9IrHFU_ID1khGvx9sA"
 
 html = requests.get(channel + "/videos").text
-url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
+url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html)
 
 
 ytvid = print(url) 
