19456ef8375a3b17d48275425516ccb159ffd9ab
Chromulent <75396296+Chromulent@users.noreply.github.com>
2023-03-03 16:56:14 -0600

    12023.03.03 @ 1655

---
+++ Automation/Download/Underactive Functions/Creator_DownloadV2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+Creator_Download () {
+    echo yt-dlp --format "'$dlpformat'" --verbose --force-ipv4 --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive "'$dlytDB'" --check-formats --concurrent-fragments 5  -o "'$subdirout'" --restrict-filenames  --embed-metadata --write-thumbnail  --throttled-rate 100K "'$videoVar'"  >> "$contentFile"
+    echo -en '\n' >> "$contentFile"
+}
+
