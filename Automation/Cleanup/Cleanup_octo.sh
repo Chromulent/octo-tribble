ac880fa4ab130e241adf3164a01868bf9e6d6f85
Chromulent <it.jumpincrow@gmail.com>
2023-03-17 08:42:46 -0500

    12023.03.17 @ 0842

---
+++ Automation/Cleanup/Cleanup_octo.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+Cleanup_octo () {
+
+	setsOctoTV="/mnt/octo2/sets/tv/"
+	setsOctoFilm="/mnt/octo2/sets/film"
+	setsOctoAnimated="/mnt/octo2/sets/animated/"
+	setsOctoAnime="/mnt/octo2/sets/anime/"
+
+	dirPart=$(pwd)
+	serName="$(cut -c 14- <<< $dirPart)"
+
+	echo "What directory are we targeting?"
+
+	read sets
+
+	case "$sets" in
+		"tv")        dirFull=${setsOctoTV}${serName};;
+		"film")      dirFull=${setsOctoFilm};;
+		"animated")  dirFull=${setsOctoAnimated}${serName};;
+		"anime")     dirFull=${setsOctoAnime}${serName};;
+		#"animated") dirFull=${setsOcto}${serName};;
+	esac
+
+	[ ! -d "$dirFull" ] && mkdir "${dirFull}"
+
+	m4varray="$(find . -name '*.m4v')"; 
+	mkvarray="$(find . -name '*.mkv')"; 
+	mp4array="$(find . -name '*.mp4')"; 
+	aviarray="$(find . -name '*.avi')"; 
+
+	if [ ${#m4varray[@]} -gt 0 ]; then find . -type f -name "*.m4v" -exec mv {} "${dirPart}" \;; fi
+	if [ ${#mkvarray[@]} -gt 0 ]; then find . -type f -name "*.mkv" -exec mv {} "${dirPart}" \;; fi
+	if [ ${#mp4array[@]} -gt 0 ]; then find . -type f -name "*.mp4" -exec mv {} "${dirPart}" \;; fi
+	if [ ${#aviarray[@]} -gt 0 ]; then find . -type f -name "*.avi" -exec mv {} "${dirPart}" \;; fi
+
+	# remove all possible files that are with all possible subfolders
+	find . -mindepth 1 -type f -name "*.sub" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.idx" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.srt" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.txt" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.exe" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.jpg" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.nfo" -exec rm -rf {} \; 
+
+	# delete all empty folders within the directory that we are performing this action within 
+	find . -empty -type d -delete
+
+	clear
+
+	if [ ${#m4varray[@]} -gt 0 ]; then mv *.m4v "${dirFull}" ; fi
+	if [ ${#mkvarray[@]} -gt 0 ]; then mv *.mkv "${dirFull}" ; fi
+	if [ ${#mp4array[@]} -gt 0 ]; then mv *.mp4 "${dirFull}" ; fi
+	if [ ${#aviarray[@]} -gt 0 ]; then mv *.avi "${dirFull}" ; fi
+
+}
+
+# Cleanup_octo
---
+++ Automation/Cleanup/Cleanup_piOS.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+Cleanup_piOS () {
+
+	## Move all of a specific extension to the main directory -- This will help with series clean up
+	find . -mindepth 1 -type f -name "*.m4v" -exec mv {} . \; 
+	find . -mindepth 1 -type f -name "*.mkv" -exec mv {} . \; 
+	find . -mindepth 1 -type f -name "*.mp4" -exec mv {} . \; 
+	find . -mindepth 1 -type f -name "*.avi" -exec mv {} . \; 
+
+
+	# remove all possible files that are with all possible subfolders
+	find . -mindepth 1 -type f -name "*.sub" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.idx" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.srt" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.txt" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.exe" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.jpg" -exec rm -rf {} \; 
+	find . -mindepth 1 -type f -name "*.nfo" -exec rm -rf {} \; 
+
+
+	# delete all empty folders within the directory that we are performing this action within 
+	find . -empty -type d -delete
+
+}
+
+# Cleanup_piOS
+
