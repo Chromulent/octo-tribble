2aa37efcf6ba8c2c2b8e2af9df2b03e3637ea7f0
Chromulent <it.jumpincrow@gmail.com>
2023-03-21 14:15:06 -0500

    12023.03.21 @ 1415

--- Automation/Functions/Vid-T_v2.sh
+++ Automation/Functions/Vid-T_v2.sh
@@ -65,8 +65,82 @@ Vid-Tv2 () {
 
   #=====[ Perform Cleanup on directory
   # after the conversion if the file is too small then delete it, and if the extension matches a file that we converted from then delete the original. 
-  source <(curl -L https://raw.githubusercontent.com/Chromulent/octo-tribble/main/Automation/Functions/DeDupe-Directory.sh)
-  DeDupe-Dir
+      if_aac_m4a_del () {
+    for f in *.aac; do
+    [ -e "${f%.*}.m4a" ] && rm -rf "$f"
+    done }
+
+    if_flac_mp3_del () {
+    for f in *.flac; do
+    [ -e "${f%.*}.mp3" ] && rm -rf "$f"
+    done }
+
+    if_webp_png_del () {
+    for f in *.webp; do
+    [ -e "${f%.*}.png" ] && rm -rf "$f"
+    done }
+
+    if_3gp_mp4_del () {
+    for f in *.3gp; do
+    [ -e "${f%.*}.mp4" ] && rm -rf "$f"
+    done }
+
+    if_mov_webm_del () {
+    for f in *.mov; do
+    [ -e "${f%.*}.webm" ] && rm -rf "$f"
+    done }
+
+    if_ts_mp4_del () {
+    for f in *.ts; do
+    [ -e "${f%.*}.mp4" ] && rm -rf "$f"
+    done }
+
+    if_m4v_mkv_del () {
+    for f in *.m4v; do
+    [ -e "${f%.*}.mkv" ] && rm -rf "$f"
+    done }
+
+    if_avi_mp4_del () {
+    for f in *.avi; do
+    [ -e "${f%.*}.mp4" ] && rm -rf "$f"
+    done }
+
+    if_avi_webm_del () {
+    for f in *.avi; do
+    [ -e "${f%.*}.webm" ] && rm -rf "$f"
+    done }
+
+    if_mkv_mp4_del () {
+    for f in *.mkv; do
+    [ -e "${f%.*}.mp4" ] && rm -rf "$f"
+    done }
+
+    if_mp4_webm_del () {
+    for f in *.mp4; do
+    [ -e "${f%.*}.webm" ] && rm -rf "$f"
+    done }
+
+    if_very_smol_del () {
+    for f in *.*; do
+    fsize=$(find "$f" -printf "%s")
+    if [[ "$fsize" -lt "500000"  ]] ;
+        then rm -rf "$f" ; 
+    fi 
+    done }
+
+      if_very_smol_del
+      chmod 777 . -R
+      if_aac_m4a_del 
+      if_flac_mp3_del 
+      if_webp_png_del 
+      if_m4v_mkv_del 
+      if_3gp_mp4_del 
+      if_mov_webm_del 
+      if_ts_mp4_del 
+      if_avi_mp4_del
+      if_avi_webm_del 
+      if_mkv_mp4_del 
+      if_mp4_webm_del 
 
 }
 
