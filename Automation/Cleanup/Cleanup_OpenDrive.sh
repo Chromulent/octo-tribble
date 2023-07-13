11ee486014250665836877264f5c83bad78e5574
Chromulent <75396296+Chromulent@users.noreply.github.com>
2023-07-13 10:11:57 -0500

    12023.07.13 @ 1011

--- Automation/Cleanup/Cleanup_OpenDrive.sh
+++ Automation/Cleanup/Cleanup_OpenDrive.sh
@@ -7,6 +7,9 @@ Cleanup_OpenDrive () {
 	setsOpenDriveAnimated="/mnt/OpenDrive3/Videos/Animated/"
 	setsOpenDriveAnime="/mnt/OpenDrive3/Videos/Anime/"
 
+
+	printf "Please pick from the following list:\n\n\ttv\n\tfilm\n\tanimated\n\tanime\n\n"
+
 	dirPart=$(pwd)
 	IFS='/'
 	read -ra partPort <<< "$dirPart"
@@ -22,7 +25,7 @@ Cleanup_OpenDrive () {
 		"film")      dirFull=${setsOpenDriveFilm};;
 		"animated")  dirFull=${setsOpenDriveAnimated}${serName};;
 		"anime")     dirFull=${setsOpenDriveAnime}${serName};;
-		#"animated") dirFull=${setsOpenDrive}${serName};;
+		#"") dirFull=${setsOpenDrive}${serName};;
 	esac
 
 	[ ! -d "$dirFull" ] && mkdir "${dirFull}"
@@ -31,18 +34,23 @@ Cleanup_OpenDrive () {
 	mkvarray="$(find . -name '*.mkv')"; 
 	mp4array="$(find . -name '*.mp4')"; 
 	aviarray="$(find . -name '*.avi')"; 
+	srtarray="$(find . -name '*.srt')"; 
 	webmarray="$(find . -name '*.webm')"; 	
+	# extarray="$(find . -name '*.ext')"; 
 
 	if [ ${#m4varray[@]} -gt 0 ]; then find . -type f -name "*.m4v" -exec mv {} "${dirPart}" \;; fi
 	if [ ${#mkvarray[@]} -gt 0 ]; then find . -type f -name "*.mkv" -exec mv {} "${dirPart}" \;; fi
 	if [ ${#mp4array[@]} -gt 0 ]; then find . -type f -name "*.mp4" -exec mv {} "${dirPart}" \;; fi
 	if [ ${#aviarray[@]} -gt 0 ]; then find . -type f -name "*.avi" -exec mv {} "${dirPart}" \;; fi
+	if [ ${#aviarray[@]} -gt 0 ]; then find . -type f -name "*.avi" -exec mv {} "${dirPart}" \;; fi
+	if [ ${#srtarray[@]} -gt 0 ]; then find . -type f -name "*.srt" -exec mv {} "${dirPart}" \;; fi
 	if [ ${#webmarray[@]} -gt 0 ]; then find . -type f -name "*.webm" -exec mv {} "${dirPart}" \;; fi
+	# if [ ${#extarray[@]} -gt 0 ]; then find . -type f -name "*.ext" -exec mv {} "${dirPart}" \;; fi
 
 	# remove all possible files that are with all possible subfolders
 	find . -mindepth 1 -type f -name "*.sub" -exec rm -rf {} \; 
 	find . -mindepth 1 -type f -name "*.idx" -exec rm -rf {} \; 
-	find . -mindepth 1 -type f -name "*.srt" -exec rm -rf {} \; 
+	# find . -mindepth 1 -type f -name "*.srt" -exec rm -rf {} \;		# Removed due to missing subtitles.
 	find . -mindepth 1 -type f -name "*.txt" -exec rm -rf {} \; 
 	find . -mindepth 1 -type f -name "*.exe" -exec rm -rf {} \; 
 	find . -mindepth 1 -type f -name "*.jpg" -exec rm -rf {} \; 
@@ -57,7 +65,9 @@ Cleanup_OpenDrive () {
 	if [ ${#mkvarray[@]} -gt 0 ]; then mv *.mkv "${dirFull}" ; fi
 	if [ ${#mp4array[@]} -gt 0 ]; then mv *.mp4 "${dirFull}" ; fi
 	if [ ${#aviarray[@]} -gt 0 ]; then mv *.avi "${dirFull}" ; fi
+	if [ ${#srtarray[@]} -gt 0 ]; then mv *.srt "${dirFull}" ; fi
 	if [ ${#webmarray[@]} -gt 0 ]; then mv *.webm "${dirFull}" ; fi
+	# if [ ${#extarray[@]} -gt 0 ]; then mv *.ext "${dirFull}" ; fi
 
 }
 
